d00cc95514d4b7fda7b72f513a7e0486787841f7
joodonghyun <91519840+joodonghyun@users.noreply.github.com>
2021-10-07 12:03:02 +0900

    [기본자료형]

---
+++ C02VariaableType/src/floattype2.java
@@ -0,0 +1,16 @@
+
+public class floattype2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float f1 = 123450000.0f;
+		float f2 = 1.2345E8f;
+		
+		System.out.println("f1 : " + f1);
+		System.out.println("f2 : " + f2);
+		
+
+	}
+
+}
